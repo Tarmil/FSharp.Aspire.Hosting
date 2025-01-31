6c1fa87114c3624fb236f6546b7c6f087bc5455d
Loïc Denuzière <loic@denuziere.net>
2025-01-31 18:56:46 +0100

    Initial commit

---
+++ sample/SampleAppHost/Program.fs
@@ -0,0 +1,12 @@
+﻿open Aspire.Hosting
+
+let builder = DistributedApplication.CreateBuilder()
+
+let service = builder.AddProject<Projects.SampleWebService>("service")
+
+let webapp =
+    builder.AddProject<Projects.SampleWebApp>("web-app")
+        .WithReference(service)
+        .WaitFor(service)
+
+builder.Build().Run()
---
+++ src/FSharp.Aspire.Hosting.AppHost/ResourceBuilderExtensions.fs
@@ -0,0 +1,56 @@
+﻿namespace Aspire.Hosting
+
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+open Aspire.Hosting.ApplicationModel
+
+// Note: We can't put all these methods in the same class,
+// because the two WithReference methods differ only by their constraints,
+// and therefore can't be overloads of one another.
+
+type ResourceBuilderWithServiceDiscoveryExtensions =
+
+    [<Extension>]
+    static member WithReference<'Destination, 'Source
+            when 'Destination :> IResourceWithEnvironment
+            and 'Source :> IResourceWithServiceDiscovery>
+        (
+            builder: IResourceBuilder<'Destination>,
+            source: IResourceBuilder<'Source>
+        ) : IResourceBuilder<'Destination> =
+        builder.WithReference(source :?> IResourceBuilder<IResourceWithServiceDiscovery>)
+
+type ResourceBuilderWithConnectionStringExtensions =
+
+    [<Extension>]
+    static member WithReference<'Destination, 'Source
+            when 'Destination :> IResourceWithEnvironment
+            and 'Source :> IResourceWithConnectionString>
+        (
+            builder: IResourceBuilder<'Destination>,
+            source: IResourceBuilder<'Source>
+        ) : IResourceBuilder<'Destination> =
+        builder.WithReference(source :?> IResourceBuilder<IResourceWithConnectionString>)
+
+type ResourceBuilderWithWaitSupportExtensions =
+
+    [<Extension>]
+    static member WaitFor<'Destination, 'Source
+            when 'Destination :> IResourceWithWaitSupport
+            and 'Source :> IResource>
+        (
+            builder: IResourceBuilder<'Destination>,
+            source: IResourceBuilder<'Source>
+        ) : IResourceBuilder<'Destination> =
+        builder.WaitFor(source :?> IResourceBuilder<IResource>)
+
+    [<Extension>]
+    static member WaitForCompletion<'Destination, 'Source
+            when 'Destination :> IResourceWithWaitSupport
+            and 'Source :> IResource>
+        (
+            builder: IResourceBuilder<'Destination>,
+            source: IResourceBuilder<'Source>,
+            [<Optional; DefaultParameterValue 0>] exitCode: int
+        ) : IResourceBuilder<'Destination> =
+        builder.WaitForCompletion(source :?> IResourceBuilder<IResource>, exitCode)
