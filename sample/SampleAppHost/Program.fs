3db4cdd53143f9b062fb06ae36d5e8888e9a1dfb
Loïc Denuzière <loic@denuziere.net>
2025-02-22 15:50:46 +0100

    feat: Add Azure Bicep integration

--- sample/SampleAppHost/Program.fs
+++ sample/SampleAppHost/Program.fs
@@ -9,10 +9,19 @@ let awsStack =
     builder.AddAWSCloudFormationStack("aws-stack")
         .WithReference(awsConfig)
 
+let sqlServer =
+    builder.AddSqlServer("sql-server")
+
+let azureStack =
+    builder.AddBicepTemplateString("azure-stack", "stack.bicep")
+        .WithParameter("sqlServer", sqlServer)
+
 let service =
     builder.AddProject<Projects.SampleWebService>("service")
         .WithReference(awsConfig)
         .WithReference(awsStack)
+        .WaitFor(azureStack)
+        .WithEnvironment("AZURE_SERVICE_URL", azureStack.GetOutput("azureServiceUrl"))
 
 let webapp =
     builder.AddProject<Projects.SampleWebApp>("web-app")
---
+++ src/FSharp.Aspire.Hosting.Azure/ResourceBuilderExtensions.fs
@@ -0,0 +1,18 @@
+﻿namespace Aspire.Hosting
+
+open System.Runtime.CompilerServices
+open Aspire.Hosting.ApplicationModel
+open Aspire.Hosting.Azure
+
+type AzureBicepResourceExtensions =
+
+    [<Extension>]
+    static member WithParameter<'Destination, 'Source
+            when 'Destination :> AzureBicepResource
+            and 'Source :> IResourceWithConnectionString>
+        (
+            builder: IResourceBuilder<'Destination>,
+            name: string,
+            source: IResourceBuilder<'Source>
+        ) : IResourceBuilder<'Destination> =
+        builder.WithParameter(name, source :?> IResourceBuilder<IResourceWithConnectionString>)
