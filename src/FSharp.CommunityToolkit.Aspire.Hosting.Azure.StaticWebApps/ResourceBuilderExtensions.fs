0d1c4d9dcb89e9b9e0c9e5ee5990c00e7fb44b3e
Loïc Denuzière <loic@denuziere.net>
2025-02-22 21:39:43 +0100

    feat: add Azure StaticWebApps integration

---
+++ src/FSharp.CommunityToolkit.Aspire.Hosting.Azure.StaticWebApps/ResourceBuilderExtensions.fs
@@ -0,0 +1,24 @@
+﻿namespace Aspire.Hosting
+
+open System.Runtime.CompilerServices
+open Aspire.Hosting.ApplicationModel
+
+type AzureStaticWebAppsExtensions =
+
+    [<Extension>]
+    static member WithAppResource<'Source
+            when 'Source :> IResourceWithEndpoints>
+        (
+            builder: IResourceBuilder<SwaResource>,
+            source: IResourceBuilder<'Source>
+        ) : IResourceBuilder<SwaResource> =
+        builder.WithAppResource(source :?> IResourceBuilder<IResourceWithEndpoints>)
+
+    [<Extension>]
+    static member WithApiResource<'Source
+            when 'Source :> IResourceWithEndpoints>
+        (
+            builder: IResourceBuilder<SwaResource>,
+            source: IResourceBuilder<'Source>
+        ) : IResourceBuilder<SwaResource> =
+        builder.WithApiResource(source :?> IResourceBuilder<IResourceWithEndpoints>)
