b9f83890ca77077c464925eef9d375534a5bec9a
Loïc Denuzière <ldenuziere@d-edge.com>
2025-03-19 20:42:36 +0100

    refactor: test script formatting

--- test.sh
+++ test.sh
@@ -7,21 +7,30 @@ cd tests
 ### Generate manifest from the sample project
 
 mkdir -p actual/sample-output
-dotnet run --project ../sample/SampleAppHost --launch-profile http -- \
-    --publisher manifest --output-path "$PWD/actual/sample-output/aspire-manifest.json"
+dotnet run \
+    --project ../sample/SampleAppHost \
+    --launch-profile http \
+    -- \
+    --publisher manifest \
+    --output-path "$PWD/actual/sample-output/aspire-manifest.json"
 
 ### Generate manifest from the project template
 
 mkdir -p actual/template-output
-if dotnet new list fsharp-aspire-apphost 2>&1 >/dev/null; then
+if dotnet new list fsharp-aspire-apphost --verbosity quiet; then
     dotnet new uninstall FSharp.Aspire.ProjectTemplates
 fi
 dotnet new install ../bin/FSharp.Aspire.ProjectTemplates.*.nupkg
 rm -rf work
 dotnet new fsharp-aspire-apphost -o work/TemplateInstance
-dotnet add work/TemplateInstance package FSharp.Aspire.Hosting.AppHost -s "$(dirname $PWD)/bin"
-dotnet run --project work/TemplateInstance --launch-profile http -- \
-    --publisher manifest --output-path "$PWD/actual/template-output/aspire-manifest.json"
+dotnet add work/TemplateInstance package FSharp.Aspire.Hosting.AppHost \
+    -s "$(dirname $PWD)/bin"
+dotnet run \
+    --project work/TemplateInstance \
+    --launch-profile http \
+    -- \
+    --publisher manifest \
+    --output-path "$PWD/actual/template-output/aspire-manifest.json"
 
 ### Check the generated files
 
